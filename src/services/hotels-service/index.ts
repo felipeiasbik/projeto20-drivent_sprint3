03f78edf2e6c57045aa64af491d802714597a465
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-07-14 19:51:30 -0300

    code modification for HUB estimator

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -4,7 +4,7 @@ import hotelsRepository from "../../repositories/hotels-repository";
 import ticketTypeRepository from "../../repositories/ticket-type-repository";
 import ticketsRepository from "../../repositories/tickets-repository";
 
-async function validationErrors(userId: number){
+async function getHotels(userId: number){
 
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
     if (!enrollment) throw notFoundError();
@@ -20,21 +20,28 @@ async function validationErrors(userId: number){
         ){
             throw paymentRequired();
         };
-}
-
-async function getHotels(userId: number){
-
-    validationErrors(userId);
    
     const hotels = await hotelsRepository.getHotels();
     if (hotels.length === 0) throw notFoundError();
-
     return hotels;
 }
 
 async function getRoomsHotelsId(userId: number, hotelId: number){
 
-    validationErrors(userId);
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if (!enrollment) throw notFoundError();
+
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+    if (!ticket) throw notFoundError();
+
+    const ticketType = await ticketTypeRepository.findTicketTypeByTicketId(ticket.ticketTypeId);
+    if (
+        ticket.status !== "PAID" ||
+        ticketType.includesHotel === false || 
+        ticketType.isRemote === true
+        ){
+            throw paymentRequired();
+        };
    
     const hotels = await hotelsRepository.getHotels();
     if (hotels.length === 0) throw notFoundError();
